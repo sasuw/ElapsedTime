cae9fbd7e9253245abdd4f0a3e63f8aaa62f585d
Sasu <info@sasu.net>
2017-11-26 20:23:53 +0100

    BasicEstimator rounding bug fixed

--- test/net/sasu/lib/gate/estimator/BasicEstimatorTest.java
+++ test/net/sasu/lib/gate/estimator/BasicEstimatorTest.java
@@ -15,16 +15,25 @@ public class BasicEstimatorTest {
     public void getRemainingTimeTest(){
         BasicEstimator be = new BasicEstimator();
         final MockTimer mockTimer = new MockTimer();
+        final long totalWorkUnits = 10;
 
-        be.init(10);
+        be.init(totalWorkUnits);
         be.setTimer(mockTimer);
 
         Assertions.assertEquals(0, be.getElapsedTime(TimeUnit.NANOSECONDS));
         Assertions.assertEquals(-1 , be.getRemainingTime(TimeUnit.NANOSECONDS));
 
+        for(int i = 1; i < totalWorkUnits; i++) {
+            mockTimer.increment();
+            be.completeWorkUnits(1);
+            Assertions.assertEquals(i, be.getElapsedTime(TimeUnit.NANOSECONDS));
+            Assertions.assertEquals(totalWorkUnits - i, be.getRemainingTime(TimeUnit.NANOSECONDS));
+        }
+
         mockTimer.increment();
         be.completeWorkUnits(1);
-        Assertions.assertEquals(1, be.getElapsedTime(TimeUnit.NANOSECONDS));
-        Assertions.assertEquals(9 , be.getRemainingTime(TimeUnit.NANOSECONDS));
+
+        Assertions.assertEquals(totalWorkUnits, be.getElapsedTime(TimeUnit.NANOSECONDS));
+        Assertions.assertEquals(0 , be.getRemainingTime(TimeUnit.NANOSECONDS));
     }
 }
