ba8c249c313c8032c0880dab1d28f846555f5351
Sasu <git@sasu.net>
2018-04-22 21:02:01 +0200

    Add unit tests.

--- ElapsedTime/src/test/java/net/sasu/lib/elapsedtime/util/StringUtilTest.java
+++ ElapsedTime/src/test/java/net/sasu/lib/elapsedtime/util/StringUtilTest.java
@@ -1,6 +1,8 @@
 package net.sasu.lib.elapsedtime.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,4 +14,21 @@ public class StringUtilTest {
         String b = StringUtil.padLeft(str, 'x', 5);
         assertEquals("xxxxa", b);
     }
+
+    @Test
+    public void padLeftTestExceptionIae1() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                ()->{StringUtil.padLeft(null, 'a', 5);} );
+        assertNotNull(exception);
+    }
+
+    @Test
+    public void padLeftTestExceptionIae2() {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                ()->{StringUtil.padLeft("aa", 'a', 0);} );
+        assertNotNull(exception);
+    }
+
+
+
 }
